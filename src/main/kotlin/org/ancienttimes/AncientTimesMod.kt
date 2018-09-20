8df3ba65fcff47e232dc5590b8161a34a227f95e
Sintorion <Sinthorion@users.noreply.github.com>
2018-09-20 23:54:36 +0200

    Minimal working version

---
+++ src/main/kotlin/org/ancienttimes/AncientTimesMod.kt
@@ -0,0 +1,6 @@
+package org.ancienttimes
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(modid="ancienttimes", name="Ancient Times", version="0.1.0", modLanguageAdapter="net.shadowfacts.forgelin.KotlinAdapter")
+object AncientTimesMod
